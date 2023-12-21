77fe5f1801c510d17aa15a0770efa3d34894ac58
Zeyad Habib <zeyadhhabib@gmail.com>
2023-12-21 20:15:55 +0000

    uncomment login routes

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -5,6 +5,6 @@ pub mod task_manager;
 
 pub fn get_router() -> Router {
     Router::new()
-        // .nest("/login", login::get_router())
+        .nest("/login", login::get_router())
         .nest("/", task_manager::get_router().merge(login::get_router()))
 }
